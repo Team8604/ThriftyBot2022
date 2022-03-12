695a26eff20cc10fcf371eef15483f682b4e58be
charles-bruel <csbruel@gmail.com>
2022-03-12 15:57:08 -0500

    Added most basic ball tunnel and dispenser commands.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,10 +3,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.subsystems.BallTunnel;
+import frc.robot.subsystems.Dispenser;
 import frc.robot.subsystems.KOPChassis;
 
 public class RobotContainer {
   public static final KOPChassis chassis = new KOPChassis();
+  public static final BallTunnel ballTunnel = new BallTunnel();
+  public static final Dispenser dispenser = new Dispenser();
+
   public static Joystick driverController = new Joystick(0);
 
   public RobotContainer() {
