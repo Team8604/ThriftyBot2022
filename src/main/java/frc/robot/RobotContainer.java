817485f4d44a67281ff936b7f44db886ab4aaf29
charles-bruel <csbruel@gmail.com>
2022-03-12 15:59:43 -0500

    Bound ball tunnel to driver bumpers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.BallTunnelMotor;
 import frc.robot.subsystems.BallTunnel;
 import frc.robot.subsystems.Dispenser;
 import frc.robot.subsystems.KOPChassis;
@@ -20,7 +22,19 @@ public class RobotContainer {
     chassis.setDefaultCommand(new ArcadeDrive());
   }
 
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    JoystickButton driverAButton = new JoystickButton(driverController, Constants.kButtonA);
+    JoystickButton driverBButton = new JoystickButton(driverController, Constants.kButtonB);
+    JoystickButton driverXButton = new JoystickButton(driverController, Constants.kButtonX);
+    JoystickButton driverYButton = new JoystickButton(driverController, Constants.kButtonY);
+    JoystickButton driverRBumper = new JoystickButton(driverController, Constants.kBumperR);
+    JoystickButton driverLBumper = new JoystickButton(driverController, Constants.kBumperL);
+  
+    driverLBumper.whenPressed(new BallTunnelMotor(-Constants.kBallTunnelSpeed));
+    driverLBumper.whenReleased(new BallTunnelMotor(0));
+    driverRBumper.whenPressed(new BallTunnelMotor(Constants.kBallTunnelSpeed));
+    driverRBumper.whenReleased(new BallTunnelMotor(0));
+  }
 
   public Command getAutonomousCommand() {
     return null;
