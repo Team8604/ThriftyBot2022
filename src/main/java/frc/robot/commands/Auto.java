9dd71d6b741c2efcd69084495a84bd580aabcb3b
charles-bruel <79763436+charles-bruel@users.noreply.github.com>
2022-03-24 11:39:39 -0400

    Add comments to Auto.java

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -9,6 +5,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
+//For some strange reason, automatic drivetrain action refuses to work when being executed from a SequentialCommandGroup.
+//Therefore, the entire auto routine has been put into a single file. It sacrifices readability, but it works.
 public class Auto extends CommandBase {
 
   private Timer timer;
